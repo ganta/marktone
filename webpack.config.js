4a33bfeb5aa615f2b9e3820851dc8796078694cb
Hideki Igarashi <hideki.develop@gmail.com>
2019-06-21 12:57:18 +0900

    chore: Add webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  devtool: "inline-source-map",
+
+  entry: {
+    content:  "./src/app/content.ts"
+  },
+
+  output: {
+    path: path.resolve(__dirname, 'dist/js'),
+    filename: '[name].js'
+  },
+
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"]
+  },
+
+  module: {
+    rules: [
+      { test: /\.tsx?$/, loader: "ts-loader" },
+      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
+    ]
+  }
+};
