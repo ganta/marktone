bed9fbf7814fc2d0e3bf28ef750665d49b02ddbe
Hideki Igarashi <hideki.develop@gmail.com>
2019-06-25 18:34:40 +0900

    style: Format webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,35 @@
 const path = require('path');
 
 module.exports = {
-  mode: "development",
-  devtool: "inline-source-map",
+    mode: "development",
+    devtool: "inline-source-map",
 
-  entry: {
-    content:  "./src/app/content.ts"
-  },
+    entry: {
+        content: "./src/app/content.ts"
+    },
 
-  output: {
-    path: path.resolve(__dirname, 'dist/js'),
-    filename: '[name].js'
-  },
+    output: {
+        path: path.resolve(__dirname, "dist/js"),
+        filename: "[name].js"
+    },
 
-  resolve: {
-    extensions: [".ts", ".tsx", ".js"]
-  },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"]
+    },
 
-  module: {
-    rules: [
-      { test: /\.tsx?$/, loader: "ts-loader" },
-      { test: /\.css$/, use: ['style-loader', 'css-loader'] }
-    ]
-  }
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                loader: "ts-loader"
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    "style-loader",
+                    "css-loader"
+                ]
+            }
+        ]
+    }
 };
