f634e8b3c588629b217302c6bea6960ac5f9c42b
Hideki Igarashi <hideki.develop@gmail.com>
2019-07-04 16:35:28 +0900

    feat: implement `delegateEvent` function

--- src/app/content.ts
+++ src/app/content.ts
@@ -1,1 +1,26 @@
 import '../styles/content.scss';
+
+function delegateEvent(
+    element: Document | HTMLElement,
+    eventName: string,
+    selector: string,
+    callback: (evt: Event, elem: HTMLElement) => void,
+): void {
+    element.addEventListener(eventName, (event: Event): void => {
+        const targetElement = event.target as HTMLElement;
+        const specifiedElement = targetElement.closest(selector) as HTMLElement;
+        if (element) {
+            callback(event, specifiedElement);
+        }
+    });
+}
+
+delegateEvent(
+    document,
+    'click',
+    'form[class$="-ui-comments-commentform-form"',
+    (event, element): void => {
+        // eslint-disable-next-line no-console
+        console.log(event, element);
+    },
+);
