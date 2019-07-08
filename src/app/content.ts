f92f0d3ee5ba4e8b385bbba37210f9f20f871eff
Hideki Igarashi <hideki.develop@gmail.com>
2019-07-08 12:32:13 +0900

    feat: Change to insert 'marktone' elemnt instead of textarea directly

--- src/app/content.ts
+++ src/app/content.ts
@@ -15,23 +15,27 @@ function delegateEvent(
     });
 }
 
-delegateEvent(
-    document,
-    'click',
-    'form[class$="-ui-comments-commentform-form"',
-    (event, element): void => {
-        // eslint-disable-next-line no-console
-        console.log(event, element);
-        const opened = element.querySelector('textarea[class="marktone-textarea"');
-        if (opened) {
-            return;
-        }
+function hasMarktone(element: HTMLElement): boolean {
+    const list = element.getElementsByClassName('marktone');
+    return list.length > 0;
+}
 
-        const textArea = document.createElement('textarea');
-        textArea.classList.add('marktone-textarea');
-        const textAreaWrapper = element.querySelector('div[class$="-ui-comments-commentform-textarea-wrap"');
-        if (textAreaWrapper) {
-            textAreaWrapper.appendChild(textArea);
-        }
-    },
-);
+function addMarktone(event: Event, element: HTMLElement): void {
+    // eslint-disable-next-line no-console
+    console.log(event, element);
+
+    if (hasMarktone(element)) {
+        return;
+    }
+
+    const marktone = document.createElement('div');
+    marktone.classList.add('marktone');
+
+    const textArea = document.createElement('textarea');
+    textArea.classList.add('marktone-textarea');
+
+    marktone.appendChild(textArea);
+    element.insertAdjacentElement('afterbegin', marktone);
+}
+
+delegateEvent(document, 'click', 'form[class$="-ui-comments-commentform-form"', addMarktone);
