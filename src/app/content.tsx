46750e2254acd923298e9705f13e332d4a531ca8
Hideki Igarashi <hideki.develop@gmail.com>
2019-07-23 20:14:19 +0900

    feat: Close Marktone if the original form is closed

--- src/app/content.tsx
+++ src/app/content.tsx
@@ -25,14 +25,30 @@ function addMarktone(event: Event, formElement: HTMLElement): void {
     console.log(event, formElement);
 
     let marktoneContainer = formElement.querySelector('div.marktone-container') as HTMLElement;
-    if (marktoneContainer === null) {
-        marktoneContainer = document.createElement('div');
-        marktoneContainer.classList.add('marktone-container');
-        formElement.prepend(marktoneContainer);
-    }
+    if (marktoneContainer !== null) { return; } // Do nothing if the container already exists.
 
     const originalEditorField = formElement.querySelector('div.ocean-ui-editor-field[role="textbox"]') as HTMLElement;
+
+    // Create Marktone Container.
+    marktoneContainer = document.createElement('div');
+    marktoneContainer.classList.add('marktone-container');
+    formElement.prepend(marktoneContainer);
+
+    // First rendering.
     ReactDOM.render(<Marktone rawText="" originalEditorField={originalEditorField} />, marktoneContainer);
+
+    // Toggle opening and closing of Marktone according to the expansion state of the original form.
+    const formExpandedObserver = new MutationObserver(() => {
+        const originalCommentContainer = formElement.parentElement as HTMLElement;
+        const isOriginalFormExpanded = (originalCommentContainer.getAttribute('aria-expanded') === 'true');
+
+        if (isOriginalFormExpanded) {
+            ReactDOM.render(<Marktone rawText="" originalEditorField={originalEditorField} />, marktoneContainer);
+        } else {
+            ReactDOM.unmountComponentAtNode(marktoneContainer);
+        }
+    });
+    formExpandedObserver.observe(formElement.parentElement as Node, { attributes: true, attributeFilter: ['aria-expanded'] });
 }
 
 delegateEvent(document, 'click', 'form.ocean-ui-comments-commentform-form', addMarktone);
