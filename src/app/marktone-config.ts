f9aeb64aabc15ebde298eb415ca4458dee8b118d
Hideki Igarashi <hideki.develop@gmail.com>
2020-01-18 04:55:11 +0900

    feat: Persist Marktone enabled status to Chrome storage

---
+++ src/app/marktone-config.ts
@@ -0,0 +1,25 @@
+class MarktoneConfig {
+  static loadEnabled(func: (enabled: boolean) => void): void {
+    chrome.storage.sync.get(["marktoneEnabled"], result => {
+      let enabled = result.marktoneEnabled;
+
+      if (enabled === undefined) enabled = true;
+
+      func(enabled);
+    });
+  }
+
+  static saveEnabled(enabled: boolean): void {
+    chrome.storage.sync.set({ marktoneEnabled: enabled });
+  }
+
+  static onEnabledChanged(func: (enabled: boolean) => void): void {
+    chrome.storage.onChanged.addListener((changes, namespace) => {
+      if (changes.marktoneEnabled) {
+        func(changes.marktoneEnabled.newValue);
+      }
+    });
+  }
+}
+
+export default MarktoneConfig;
