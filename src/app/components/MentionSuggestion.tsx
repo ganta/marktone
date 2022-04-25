2211433e01c9a43d99a5e2e6f9a0859336916f0a
Hideki Igarashi <hideki.develop@gmail.com>
2022-04-25 16:40:26 +0900

    [from now] 2022/04/25 16:40:26 [ci skip]
     src/app/components/MentionSuggestion.stories.tsx | 13 +++--
     src/app/components/MentionSuggestion.tsx         | 65 ++++++++++++++++++++----
     2 files changed, 66 insertions(+), 12 deletions(-)

--- src/app/components/MentionSuggestion.tsx
+++ src/app/components/MentionSuggestion.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 
 type SuggestedEntity = {
@@ -10,28 +10,75 @@ type SuggestedEntity = {
 };
 
 const Component = styled.div`
-  border: 1px solid red;
+  border-width: 1px;
+  border-style: solid;
+  border-color: #ccc;
+  border-radius: 4px;
+  box-shadow: rgba(0, 0, 0, 0.1) 0 4px 12px 0;
+  max-height: 200px;
+  overflow-x: hidden;
+  overflow-y: auto;
 `;
 
-const ItemList = styled.ul``;
+const ItemList = styled.ul`
+  list-style: none;
+  margin: 6px 0;
+  padding: 0;
+`;
 
-const Item = styled.li``;
+const Item = styled.li<{ selected: boolean }>`
+  display: flex;
+  flex-direction: row;
+  line-height: 20px;
+  font-size: 14px;
+  padding: 4px 8px;
+  background-color: ${({ selected }) => (selected ? "#0c50cd" : "transparent")};
+  color: ${({ selected }) => (selected ? "#fff" : "#000")};
+`;
 
-const Avatar = styled.img``;
+const AvatarImage = styled.img`
+  width: 100%;
+  height: 100%;
+  border-radius: 50%;
+`;
+
+const Avatar = styled.span`
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+`;
+
+const Name = styled.span`
+  font-weight: bold;
+  margin: 0 8px;
+`;
+
+const Code = styled.span`
+  opacity: 0.75;
+`;
 
 type Props = {
   suggestedEntities: SuggestedEntity[];
 };
 
 const MentionSuggestion = ({ suggestedEntities }: Props) => {
+  const [selectedEntity, _selectEntity] = useState(suggestedEntities[0]);
+
   return (
     <Component>
       <ItemList>
-        {suggestedEntities.map(({ entityType, id, code, name, avatarUrl }) => {
+        {suggestedEntities.map((entity) => {
+          const { entityType, id, code, name, avatarUrl } = entity;
+
           return (
-            <Item key={`${entityType}-${id}`}>
-              <Avatar src={avatarUrl} />
-              {name} {code}
+            <Item
+              key={`${entityType}-${id}`}
+              selected={entity === selectedEntity}
+            >
+              <Avatar>
+                <AvatarImage src={avatarUrl} alt="" />
+              </Avatar>
+              <Name>{name}</Name> <Code>{code}</Code>
             </Item>
           );
         })}
