a0da0fe4f115954569307cfc960cba4d4735d660
Hideki Igarashi <hideki.develop@gmail.com>
2023-11-04 01:39:45 +0900

    fix: handle the returned Promise

--- src/app/background.ts
+++ src/app/background.ts
@@ -4,7 +4,7 @@ function setExtensionIcon(enabled: boolean): void {
   const path = chrome.runtime.getURL(
     enabled ? "icons/icon48.png" : "icons/disabled-icon48.png"
   );
-  chrome.action.setIcon({ path });
+  chrome.action.setIcon({ path }).catch(console.error);
 }
 
 MarktoneConfig.loadEnabled((enabled) => {
