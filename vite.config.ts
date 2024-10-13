d882a9e679afcf238eee1d35a03d6d072b2706ea
Hideki Igarashi <hideki.develop@gmail.com>
2024-10-13 23:39:34 +0900

    chore: use `names` instead of the deprecated `name` of `PreRenderedAsset`

--- vite.config.ts
+++ vite.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
             : "assets/[name]-[hash].js";
         },
         assetFileNames: (assetInfo) => {
-          if (assetInfo.name === "content.css") {
+          if (assetInfo.names?.includes("content.css")) {
             return "content.css";
           }
           return "assets/[name]-[hash][extname]";
