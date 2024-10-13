d0158c38616168c668adcbd282024830e9403be4
Hideki Igarashi <hideki.develop@gmail.com>
2024-10-13 23:14:51 +0900

    chore: install @vitejs/plugin-react-swc

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,10 @@
 import { resolve } from "node:path";
 
+import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
+  plugins: [react()],
   publicDir: "public",
   build: {
     outDir: "dist",
