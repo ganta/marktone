d4910624efc7379e19418707e293d015f189c9fc
Hideki Igarashi <hideki.develop@gmail.com>
2024-10-26 22:37:54 +0900

    chore: set up Vite for vanilla-extract

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
 /// <reference types="vitest/config" />
 import { resolve } from "node:path";
 
+import { vanillaExtractPlugin } from "@vanilla-extract/vite-plugin";
 import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), vanillaExtractPlugin()],
   publicDir: "public",
   build: {
     outDir: "dist",
