e941140dd3292ca73ed102c0efc5ed45c20576d1
Hideki Igarashi <hideki.develop@gmail.com>
2024-10-26 23:54:03 +0900

    chore: use vanilla-extract in `preview.ts`

---
+++ .storybook/preview.css.ts
@@ -0,0 +1,6 @@
+import { globalStyle } from "@vanilla-extract/css";
+
+globalStyle("body", {
+  fontFamily: "'Hiragino Kaku Gothic ProN', Meiryo, sans-serif",
+  fontSize: "14px",
+});
--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,4 +1,4 @@
-import "./preview.scss";
+import "./preview.css.ts";
 import type { Preview } from "@storybook/react";
 
 const preview: Preview = {
